450cc1e485dd48bdaba31ca829971b7170d5a5b5
Jair Beltran <Jair_921@hotmail.com>
2019-11-13 20:24:45 -0500

    Aplicacion de material-ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import WeatherLocation from './components/WeatherLocation';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <WeatherLocation></WeatherLocation>
-      </div>
+      <MuiThemeProvider>
+        <div className="App">
+          <WeatherLocation></WeatherLocation>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react';
+import CircularProgress from 'material-ui/CircularProgress'
 import Location from './Location';
 import WeatherData from './WeatherData';
 import transformWeather from './../../services/transformWeather';
-import {SUN} from './../../constants/weathers';
 import './style.css';
 
 const location = "Medellín, col";
 const api_key = "1823224cb3d317ae600c62b11680423a";
 const api_weather =  `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${api_key}`
-
-const data1 = {
-    temperature: 20,
-    weatherState: SUN,
-    humidity: 10,
-    wind: '10 m/s',
-};
 
 class WeatherLocation extends Component {
 
@@ -23,7 +16,7 @@ class WeatherLocation extends Component {
         super();
         this.state = {
             city: 'Medellín',
-            data: data1
+            data: null
         };
     };
 
@@ -48,8 +41,8 @@ class WeatherLocation extends Component {
         return(
             <div className='weatherLocationCont'>
                 <Location city={city}></Location>
-                <WeatherData data={data}></WeatherData>
-                <button onClick={this.handleUpdateClick}>Actualizar</button>
+                {data ? <WeatherData data={data}/> : 
+                <CircularProgress size={60} thickness={7} />}
             </div>
         )
     };
