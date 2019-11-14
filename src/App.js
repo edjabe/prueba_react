2396f1f6db52adcf4552e1d2723b85b8b3475665
edjabe <Jair_921@hotmail.com>
2019-11-14 16:58:14 -0500

    Avances en Flex

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Paper from 'material-ui/Paper';
+import AppBar from 'material-ui/AppBar';
 import LocationList from './components/LocationList';
 import './App.css';
 
@@ -21,12 +24,27 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <div className="App">
-          <LocationList
-            cities={cities}
-            onSelectedLocation={this.handleSelectedLocation}
-          />
-        </div>
+        <Grid>
+          <Row>
+            <Col xs={12}>
+              <AppBar title="WeatherApp">
+              </AppBar>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} md={6}>
+              <LocationList
+                cities={cities}
+                onSelectedLocation={this.handleSelectedLocation}
+              />
+            </Col>
+            <Col xs={12} md={6}>
+              <Paper zDepth={4}>
+                <div className="detail"></div>
+              </Paper>
+            </Col>
+          </Row>
+        </Grid>
       </MuiThemeProvider>
     );
   }
