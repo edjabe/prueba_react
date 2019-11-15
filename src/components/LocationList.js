d10944379a61aa68507890cfd70cd00d5e1baa83
edjabe <Jair_921@hotmail.com>
2019-11-15 13:57:46 -0500

    Responsive finalizado

--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import WeatherLocation from './WeatherLocation'
+import WeatherLocation from './WeatherLocation';
+import './style.css';
 
 const LocationList = ({ cities, onSelectedLocation }) => {
     const handleWatherLocationClick = city => {
@@ -20,7 +21,7 @@ const LocationList = ({ cities, onSelectedLocation }) => {
     
 
     return (
-        <div>
+        <div className="locationList">
             {strToComponent(cities)}
         </div>
     )
